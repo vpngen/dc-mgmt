028327e710c5ed0854e869fdb68faf01c3f68bd8
Ingmund Olsson <ingmund@proton.me>
2022-12-05 01:54:51 +0200

    add: add build tar suite

---
+++ tarpkg/build-pkg.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+export CGO_ENABLED=0
+
+go install github.com/vpngen/realm-admin/cmd/delbrigade@latest
+go install github.com/vpngen/realm-admin/cmd/addbrigade@latest
+go install github.com/vpngen/realm-admin/cmd/addbrigade/gen@latest
+
+git clone ssh://git@github.com/vpngen/realm-admin
+cp realm-admin/cmd/addnet/*.sh bin/
+cp realm-admin/cmd/addpair/*.sh bin/
+cp realm-admin/cmd/*.sh bin/
+cp -r realm-admin/install .
+
+cp -f realm-deploy/src/_valera_ .
+
+rm -f /data/extract-realm.sh
+cp -f realm-deploy/src/update-realm-tpl.sh /data/update-realm.sh
+
+echo "command=\"sudo -u vgrealm -g vgrealm /opt/vgrealm/cmd/ssh_command.sh \${SSH_ORIGINAL_COMMAND}\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ${AUTH_KEY}" > authorized_keys
+
+tar -zcf - \
+        bin/delbrigade \
+        bin/addbrigade \
+        bin/gen \
+        bin/add_endpoint_net.sh \
+        bin/add_cgnat_net.sh \
+        bin/add_ula_net.sh \
+        bin/add_private_net.sh \
+        bin/add_keydesk_net.sh \
+        bin/add_pair.sh \
+        bin/ssh_command.sh \
+        install \
+        _valera_ \
+        | base64 >> /data/update-realm.sh
+
+chown ${USER_UID}:${USER_UID} /data/update-realm.sh
+
---
+++ tarpkg/src/update-realm.tpl.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+set -e
+
+DBNAME=${DBNAME:-"vgrealm"}
+REALM_ADMIN=${REALM_ADMIN:-"vgrealm"}
+SSH_API_USER=${SSH_API_USER:="_valera_"}
+
+FORCE_INSTALL=$1
+
+if [ -n ${FORCE_INSTALL} -a "x" != "x${FORCE_INSTALL}" ]; then
+        export DEBIAN_FRONTEND=noninteractive
+        apt-get update; apt-get dist-upgrade -y; apt-get autoremove -y
+        apt-get install -q -y --no-install-recommends postgresql postgresql-contrib
+
+        systemctl start --now postgresql
+fi
+
+# Extract files
+
+INSTALL_DIR="/opt/__install__"
+install -g root -o root -m 0500 -d "${INSTALL_DIR}"
+awk '/^__PAYLOAD_BEGINS__/ { print NR + 1; exit 0; }' $0 | xargs -I {} tail -n +{} $0 | base64 -d | tar -xzp -C ${INSTALL_DIR} >> /install.log 2>&1
+
+
+# Init database
+
+if [ -n $FORCE_INSTALL -a "x" != "x${FORCE_INSTALL}" ]; then
+        sudo -i -u postgres psql <<EOF
+CREATE DATABASE ${DBNAME};
+EOF
+        sudo -i -u postgres "DBNAME=${DBNAME} ${INSTALL_DIR}/install.sh"
+fi
+
+# Install realm-admin
+
+if [ -n $FORCE_INSTALL -a "x" != "x${FORCE_INSTALL}" ]; then
+        useradd -p "*" -m "${REALM_ADMIN}"
+        chmod 700 "/home/${REALM_ADMIN}"
+
+        install -o root -g "${REALM_ADMIN}" -m 0010 -d /etc/vgrealm
+        install -o root -g "${REALM_ADMIN}" -m 0010 -d /opt/vgrealm
+        install -o root -g "${REALM_ADMIN}" -m 0010 -d /opt/vgrealm/utils
+        install -o root -g "${REALM_ADMIN}" -m 0010 -d /opt/vgrealm/cmd
+
+        install -o root -g "${REALM_ADMIN}" -m 040 /opt/vgrealm/dbname
+        echo "${DBNAME}" > /etc/vgrealm/dbname
+fi
+
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/add_endpoint_net.sh" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/add_private_net.sh" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/add_cgnat_net.sh" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/add_ula_net.sh" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/add_keydesk_net.sh" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/gen" /opt/vgrealm/utils
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/ssh_command.sh" /opt/vgrealm/cmd
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/addbrigade" /opt/vgrealm/cmd
+install -o root -g "${REALM_ADMIN}" -m 050 "${INSTALL_DIR}/bin/delbrigade" /opt/vgrealm/cmd
+
+
+if [ -n $FORCE_INSTALL -a "x" != "x${FORCE_INSTALL}" ]; then
+        useradd -p "*" -m "${SSH_API_USER}"
+        chmod 700 "/home/${SSH_API_USER}"
+
+        install -g root -o root -m 644 "${INSTALL_DIR}/${SSH_API_USER}" "/etc/sudoers.d/${SSH_API_USER}"
+        install -g ${SSH_API_USER} -o ${SSH_API_USER} -m 0700 -d "/home/${SSH_API_USER}/.ssh"
+        install -g ${SSH_API_USER} -o ${SSH_API_USER} -m 600 "${INSTALL_DIR}/authorized_keys" "/home/${SSH_API_USER}/.ssh/authorized_keys"
+fi
+
+# Cleanup
+
+rm -rf "${INSTALL_DIR}"
+
+exit 0
+__PAYLOAD_BEGINS__
