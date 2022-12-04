1a09023eac5a35be3aa9527964c312e7d01ca0a1
Ingmund Olsson <ingmund@proton.me>
2022-12-05 01:58:55 +0200

    debug

--- tarpkg/build-pkg.sh
+++ tarpkg/build-pkg.sh
@@ -8,7 +8,7 @@ go install github.com/vpngen/realm-admin/cmd/delbrigade@latest
 go install github.com/vpngen/realm-admin/cmd/addbrigade@latest
 go install github.com/vpngen/realm-admin/cmd/addbrigade/gen@latest
 
-git clone ssh://git@github.com/vpngen/realm-admin
+# git clone ssh://git@github.com/vpngen/realm-admin 
 cp realm-admin/cmd/addnet/*.sh bin/
 cp realm-admin/cmd/addpair/*.sh bin/
 cp realm-admin/cmd/*.sh bin/
@@ -17,7 +17,7 @@ cp -r realm-admin/install .
 cp -f realm-deploy/src/_valera_ .
 
 rm -f /data/extract-realm.sh
-cp -f realm-deploy/src/update-realm-tpl.sh /data/update-realm.sh
+cp -f realm-admin/tarpkg/src/update-realm.tpl.sh /data/update-realm.sh
 
 echo "command=\"sudo -u vgrealm -g vgrealm /opt/vgrealm/cmd/ssh_command.sh \${SSH_ORIGINAL_COMMAND}\",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ${AUTH_KEY}" > authorized_keys
 
