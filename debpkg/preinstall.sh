f06a475bcaeff2045c06b9edc329a29468b25eb6
Ingmund Olsson <ingmund@proton.me>
2023-03-06 10:18:21 +0300

    add user install

--- debpkg/preinstall.sh
+++ debpkg/preinstall.sh
@@ -8,7 +8,6 @@ TMP_DIR="temp_dir"
 
 INSTALLATION_TEMP_DIR="${PROJECT_NAME}_${TMP_DIR}"
 
-
 REALM_ADMIN=${REALM_ADMIN:-"vgrealm"}
 SSH_API_USER=${SSH_API_USER:="_valera_"}
 
@@ -22,8 +21,27 @@ function doCheckDBRunAndStart {
     return 1
 }
 
+function doCheckUserExist {
+    expectExistUser=$1
+    usr=$(grep /etc/passwd "${expectExistUser}")
+    if [ -n "$usr" ]; then 
+	return 0
+    fi
+    return 1
+}
+
 function doInstallUsers {
-    echo "doInstallUsers"
+    echo " [=] Check or Install users"
+
+    if ! doCheckUserExist "${REALM_ADMIN}"; then 
+	useradd -p "*" -m "${REALM_ADMIN}"
+	chmod 700 "/home/${REALM_ADMIN}"
+    fi
+
+    if ! doCheckUserExist "${SSH_API_USER}"; then
+	useradd -p "*" -m "${SSH_API_USER}"
+	chmod 700 "/home/${SSH_API_USER}"
+    fi
 }
 
 function doMakeDBInitDir {
