329c6477017bdb7060074b973cf614f8dd4d441f
Ingmund Olsson <ingmund@proton.me>
2023-03-08 01:40:17 +0300

    fix stats-loop.sh

--- cmd/stats-loop.sh
+++ cmd/stats-loop.sh
@@ -9,11 +9,11 @@ update() {
 	echo "${c}"
 	${c} <<EOF
 	BEGIN;
-		INSERT INTO stats.brigades_stats (brigade_id, create_at) VALUES (:'brigade_id',to_timestamp(:create_at)) ON CONFLICT DO NOTHING;
+		INSERT INTO stats.brigades_stats (brigade_id, create_at) VALUES (:'brigade_id',:'create_at'::timestamp) ON CONFLICT DO NOTHING;
 		UPDATE 
 			stats.brigades_stats 
 		SET 
-			last_visit=CASE WHEN :last_visit = -62135596800 THEN NULL ELSE to_timestamp(:last_visit) END,
+			last_visit=CASE WHEN :'last_visit' = '0001-01-01T00:00:00Z' THEN NULL ELSE :'last_visit'::timestamp END,
 			user_count=:user_count 
 		WHERE 
 			brigades_stats.brigade_id=:'brigade_id';
@@ -21,17 +21,17 @@ update() {
 EOF
 }
 
-list=$(psql -d "${DBNAME}" -v ON_ERROR_STOP=yes -t -A -c 'SELECT pair_id FROM pairs.pairs WHERE is_active=true ORDER BY control_ip')
+list=$(psql -d ${DBNAME} -v ON_ERROR_STOP=yes -t -A -c 'SELECT pair_id FROM pairs.pairs WHERE is_active=true ORDER BY control_ip')
 
 for sid in ${list} ; do
         echo "[i] Server: ${sid}"
 
-	ip=$(psql -d "${DBNAME}" -q -v ON_ERROR_STOP=yes -t -A --set sid="${sid}" <<EOF
+	ip=$(psql -d ${DBNAME} -q -v ON_ERROR_STOP=yes -t -A --set sid=${sid} <<EOF
 SELECT control_ip FROM pairs.pairs WHERE pair_id=:'sid'
 EOF
 )
 
-	brigades=$(psql -d "${DBNAME}" -q -v ON_ERROR_STOP=yes -t -A -F " " --set sid="${sid}" <<EOF
+	brigades=$(psql -d ${DBNAME} -q -v ON_ERROR_STOP=yes -t -A -F " " --set sid=${sid} <<EOF
 SELECT brigade_id FROM brigades.brigades WHERE pair_id=:'sid'
 EOF
 )
@@ -41,27 +41,26 @@ EOF
 	done
 	list=$(echo "${list}" | sed "s/^.\{1\}//")
 
-	cmd="stats -b ${list}"
+	cmd="fetchstats -b ${list}"
 	echo "CMD: ${cmd}"
-	output=$(ssh -o IdentitiesOnly=yes -o IdentityFile=~/.ssh/id_ecdsa -o StrictHostKeyChecking=no ${USERNAME}@"${ip}" "${cmd}")
+	output=$(ssh -o IdentitiesOnly=yes -o IdentityFile=~/.ssh/id_ecdsa -o StrictHostKeyChecking=no ${USERNAME}@${ip} ${cmd})
 	rc=$?
 	if [ $rc -ne 0 ]; then 
 		echo "[-]         Something wrong: $rc"
+		echo "$output"
 		continue
 	fi
 
-	output=$(echo "${output}" | tail -n +2)
-	
+	echo "$output" > "${sid}.json"
+
 	count=$(echo "${output}" | jq -r '.stats|length')
 
 	a=0
 	while [ "$a" -lt "${count}" ]; do
-		args=$(echo "${output}" | jq -r ".stats[${a}]" | jq -r '. | "\(.brigade_id) \(.ctreate_at) \(.last_visit) \(.users_count)"')
-		update "${args}"
-		a=$(expr "$a" + 1)
+		args=$(echo "${output}" | jq -r ".stats[${a}]" | jq -r '. | "\(.brigade_id) \(.brigade_created_at) \(.keydesk_last_visit) \(.users_count)"')
+		update ${args}
+		a=$(expr $a + 1)
 	done
-
-	#args=$(echo "${output}" jq -r '.stats[] | "\(.brigade_id),\(.ctreate_at),\(.last_visit),\(.users_count)"')
 
         #exit
 done
