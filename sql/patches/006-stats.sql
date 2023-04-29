a1ca27bb0e50cec88a5cc5fb02c8c08bc231f78b
Ingmund Olsson <ingmund@proton.me>
2023-04-29 03:43:34 +0300

    fix: fix patches

---
+++ sql/patches/006-stats.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+SELECT _v.assert_user_is_superuser();
+
+SELECT _v.register_patch( '006-stats' , ARRAY[ '001-init', '002-roles', '003-stats', '004-stats', '005-stats']);
+
+ALTER TABLE :"schema_stats_name".brigades_stats ADD CONSTRAINT brigade_id_unique UNIQUE(brigade_id);
+
+GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA :"schema_stats_name" TO :"stats_dbuser";
+
+COMMIT;
