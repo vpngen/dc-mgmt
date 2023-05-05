ab61f6239ebddbf02afe848340eff4c9b0a43ff4
Ingmund Olsson <ingmund@proton.me>
2023-05-05 03:38:56 +0300

    change: ssh stderr output 2

--- cmd/delbrigade/main.go
+++ cmd/delbrigade/main.go
@@ -194,15 +194,13 @@ func revokeBrigade(db *pgxpool.Pool, schema string, sshconf *ssh.ClientConfig, b
 	session.Stderr = &e
 
 	defer func() {
-		fmt.Fprintf(os.Stderr, "%s: SSH Session StdErr:", LogTag)
-
 		switch errstr := e.String(); errstr {
 		case "":
-			fmt.Fprintln(os.Stderr, " empty")
+			fmt.Fprintf(os.Stderr, "%s: SSH Session StdErr: empty\n", LogTag)
 		default:
-			fmt.Fprintln(os.Stderr)
+			fmt.Fprintf(os.Stderr, "%s: SSH Session StdErr:\n", LogTag)
 			for _, line := range strings.Split(errstr, "\n") {
-				fmt.Fprintf(os.Stderr, "%s:    | %s\n", LogTag, line)
+				fmt.Fprintf(os.Stderr, "%s: | %s\n", LogTag, line)
 			}
 		}
 	}()
