34864c74421c6633fca434497d7fec09f9ae420c
Ingmund Olsson <ingmund@proton.me>
2023-05-23 00:45:56 +0300

    some improvements

--- cmd/ssh_command.sh
+++ cmd/ssh_command.sh
@@ -3,10 +3,6 @@
 # interpret first argument as command
 # pass rest args to scripts
 
-printdef() {
-    echo "Usage: <command> <args...>"
-    exit 1
-}
 
 if [ $# -eq 0 ]; then 
     printdef
@@ -15,18 +11,38 @@ fi
 cmd=${1}; shift
 basedir=$(dirname "$0")
 
+printdef() {
+    echo "Usage: <command> <args...>"
+    exit 1
+}
+
+kdsync() {
+        # shellcheck source=/dev/null
+        . /etc/vg-dc-vpnapi/kdsync.env
+
+        export KDSYNC_SERVER_ADDR
+        export KDSYNC_SERVER_PORT
+
+        # shellcheck source=/dev/null
+        . /etc/vg-dc-mgmt/dc-name.env
+
+        /usr/bin/flock -x -E 0 -n /tmp/kdsync.lock "${basedir}"/kdsync.sh 2>&1 | /usr/bin/logger -p local0.notice -t KDSYNC
+}
+
 if [ "addbrigade" = "${cmd}" ]; then
-    "${basedir}"/addbrigade "$@"
-    /usr/bin/flock -x -E 0 -n /tmp/kdsync.lock "${basedir}"/kdsync.sh 2>&1 | /usr/bin/logger -p local0.notice -t KDSYNC
+        "${basedir}"/addbrigade "$@"
+        kdsync
 elif [ "delbrigade" = "${cmd}" ]; then
-    "${basedir}"/delbrigade "$@"
-    /usr/bin/flock -x -E 0 -n /tmp/kdsync.lock "${basedir}"/kdsync.sh 2>&1 | /usr/bin/logger -p local0.notice -t KDSYNC
+        "${basedir}"/delbrigade "$@"
+        kdsync
 elif [ "replacebrigadier" = "${cmd}" ]; then
     "${basedir}"/replacebrigadier "$@"
 elif [ "getwasted" = "${cmd}" ]; then
     "${basedir}"/getwasted "$@"
 elif [ "checkbrigade" = "${cmd}" ]; then
     "${basedir}"/checkbrigade "$@"
+elif [ "get_free_slots" = "${cmd}" ]; then
+    "${basedir}"/get_free_slots "$@"
 else
     echo "Unknown command: ${cmd}"
     printdef
--- go.mod
+++ go.mod
@@ -10,7 +10,11 @@ require (
 	golang.org/x/crypto v0.8.0
 )
 
-require github.com/vpngen/keydesk v1.1.17
+require (
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
+	github.com/gorilla/mux v1.8.0
+	github.com/vpngen/keydesk v1.1.17
+)
 
 require (
 	github.com/alexsergivan/transliterator v1.0.0 // indirect
