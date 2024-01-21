ca22466c15a0bfee0aa58d0a6487be4f6aae3737
Ingmund Olsson <ingmund@proton.me>
2024-01-21 04:10:48 +0200

    fix

--- cmd/collectsnaps/dc-loop.sh
+++ cmd/collectsnaps/dc-loop.sh
@@ -5,11 +5,11 @@ BRIGADES_SCHEMA=${BRIGADES_SCHEMA:-"brigades"}
 
 nets=$(psql -qtA -d "${DBNAME}" \
         --set ON_ERROR_STOP=yes \
-        --set brigades_schema_name="${SCHEMA}" <<EOF
+        --set brigades_schema="${BRIGADES_SCHEMA}" <<EOF
 SELECT 
         DISTINCT(set_masklen(endpoint_ipv4,24) & '255.255.255.0'::inet) AS net24 
 FROM  
-        :"brigades_schema_name".brigades;
+        :"brigades_schema".brigades;
 EOF
 )
 
