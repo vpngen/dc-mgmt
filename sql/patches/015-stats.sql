012c3aed1b9e67966b6e9e34dbec63079e323d86
Ingmund Olsson <ingmund@proton.me>
2024-04-11 14:22:42 +0300

    fix: fix sql

--- sql/patches/015-stats.sql
+++ sql/patches/015-stats.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SELECT _v.assert_user_is_superuser();
 
-SELECT _v.register_patch( '015-stats', ARRAY[ '001-init', '002-roles', '003-stats', '004-stats', '005-stats', '006-stats', '007-stats', '008-domains', '009-domains', '010-viewfixes', '011-collectsnaps'.'012-brigades-instance','013-slot-flags'. '014-viewfixes']);
+SELECT _v.register_patch( '015-stats', ARRAY[ '001-init', '002-roles', '003-stats', '004-stats', '005-stats', '006-stats', '007-stats', '008-domains', '009-domains', '010-viewfixes', '011-collectsnaps','012-brigades-instance','013-slot-flags', '014-viewfixes']);
 
 ALTER TABLE :"schema_stats_name".brigades_stats DROP COLUMN IF EXISTS active_wg_users_count; 
 ALTER TABLE :"schema_stats_name".brigades_stats DROP COLUMN IF EXISTS active_ipsec_users_count;
