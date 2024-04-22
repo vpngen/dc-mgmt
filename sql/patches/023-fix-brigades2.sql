6711e9550dd7b24392ca40cf724ab24d214cbdf3
Ingmund Olsson <ingmund@proton.me>
2024-04-22 03:16:36 +0300

    prepare migr 10

--- sql/patches/023-fix-brigades2.sql
+++ sql/patches/023-fix-brigades2.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 SELECT _v.assert_user_is_superuser();
 
-SELECT _v.register_patch( '023-fix-brigades2', ARRAY[ '001-init', '002-roles', '003-stats', '004-stats', '005-stats', '006-stats', '007-stats', '008-domains', '009-domains', '010-viewfixes', '011-collectsnaps', '012-brigades-instance', '013-slot-flags', '014-viewfixes', '015-stats', '016-viewfixes', '017-viewfixes', '018-viewfixes', '019-ctrl-keys', '020-viewfixes', '021-vgmigr-role'. '022-fix-brigades']);
+SELECT _v.register_patch( '023-fix-brigades2', ARRAY[ '001-init', '002-roles', '003-stats', '004-stats', '005-stats', '006-stats', '007-stats', '008-domains', '009-domains', '010-viewfixes', '011-collectsnaps', '012-brigades-instance', '013-slot-flags', '014-viewfixes', '015-stats', '016-viewfixes', '017-viewfixes', '018-viewfixes', '019-ctrl-keys', '020-viewfixes', '021-vgmigr-role', '022-fix-brigades']);
 
 ALTER TABLE :"schema_brigades_name".brigades DROP CONSTRAINT unique_brigadier_per_id; 
 
